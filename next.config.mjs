3594d0b7a1cb6b2794568d198c66ff4908702092
Shubham <129274040+Anheuserly@users.noreply.github.com>
2025-12-30 15:29:50 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',        // ✅ REQUIRED for Cloudflare Pages
+       
   images: {
     unoptimized: true,     // ✅ REQUIRED (no Next/Image server)
   },
